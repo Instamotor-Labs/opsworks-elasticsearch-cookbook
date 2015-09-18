f927bc8fe52dd6d5b38ed2d789e306ffb0cb85f1
Evgeniy Skidan <eskidan@gmail.com>
2015-09-19 02:36:29 +0300

    More plugins

--- layer-custom/recipes/esplugins.rb
+++ layer-custom/recipes/esplugins.rb
@@ -8,4 +8,34 @@ script "install_plugin_es_head" do
 	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/head") }
 end
 
+script "install_plugin_es_bigdesk" do
+	interpreter "bash"
+	user "root"
+	cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
+	code <<-EOH
+  	plugin -install lukas-vlcek/bigdesk
+	EOH
+	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/bigdesk") }
+end
+
+script "install_plugin_es_kopf" do
+	interpreter "bash"
+	user "root"
+	cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
+	code <<-EOH
+  	plugin -install lmenezes/elasticsearch-kopf
+	EOH
+	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/kopf") }
+end
+
+script "install_plugin_es_paramedic" do
+	interpreter "bash"
+	user "root"
+	cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
+	code <<-EOH
+  	plugin -install karmi/elasticsearch-paramedic
+	EOH
+	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/paramedic") }
+end
+
 #notifies :restart, 'service[elasticsearch]' unless node.elasticsearch[:skip_restart]
